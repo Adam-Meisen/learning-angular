041df998b09ab3f33df579fec024012c9bf4f494
Adam <meisen.adam@gmail.com>
2016-06-24 16:46:03 -0500

    set up routing from hero list to hero detail component

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router-deprecated';
+
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
@@ -13,7 +15,9 @@ export class HeroesComponent implements OnInit{
   selectedHero: Hero;
   heroes: Hero[];
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
 
   getHeroes() {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -24,4 +28,8 @@ export class HeroesComponent implements OnInit{
   }
 
   onSelect(hero: Hero) { this.selectedHero = hero;}
+
+  goToDetail() {
+    this.router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+  }
 }
