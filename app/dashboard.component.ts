f47c26dd6a0472a369f91fc9623a6ef5631c07de
Adam <meisen.adam@gmail.com>
2016-06-24 20:14:05 -0500

    external css styles

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -7,6 +7,7 @@ import { HeroService } from './hero.service';
 @Component ({
   selector: 'my-dashboard',
   templateUrl: 'app/dashboard.component.html',
+  styleUrls: ['app/dashboard.component.css']
 })
 
 export class DashboardComponent implements OnInit{
@@ -18,7 +19,7 @@ export class DashboardComponent implements OnInit{
 
   ngOnInit() {
     this.heroService.getHeroes()
-      .then(heroes => this.heroes = heroes.slice(1,5));
+      .then(heroes => this.heroes = heroes.slice(1, 5));
   }
 
   goToDetail(hero: Hero) {
