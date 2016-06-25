fa21ad1d3e9bd992b2658531b3fa7719b90008f0
Adam <meisen.adam@gmail.com>
2016-06-24 20:57:37 -0500

    Added a bunch of methods to the hero service to save and delete heroes

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -16,11 +16,48 @@ export class HeroService {
     return Promise.reject(error.message || error);
   }
 
+  private post(hero: Hero): Promise<Hero> {
+    let headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+
+    return this.http
+               .post(this.heroesUrl, JSON.stringify(hero))
+               .toPromise()
+               .then(res => res.json().data)
+               .catch(this.handleError);
+  }
+
+  private put(hero: Hero): Promise<Hero> {
+    let headers = new Headers;
+    headers.append('Content-Type', 'application/json');
+
+    let url = `${this.heroesUrl}/${hero.id}`;
+
+    return this.http
+               .put(url, JSON.stringify(hero), {headers: headers})
+               .toPromise()
+               .then(() => hero)
+               .catch(this.handleError);
+  }
+
+  private delete(hero: Hero) {
+    let headers = new Headers;
+    headers.append('Content-Type', 'application/json');
+
+    let url = `${this.heroesUrl}/${hero.id}`;
+
+    return this.http
+               .delete(url, headers)
+               .toPromise()
+               .catch(this.handleError);
+  }
+
   getHeroes(): Promise<Hero[]> {
     return this.http.get(this.heroesUrl)
-                .toPromise()
-                .then(response => response.json().data)
-                .catch(this.handleError);
+               .toPromise()
+               .then(response => response.json().data)
+               .catch(this.handleError);
   }
 
   getHero(id: number) {
@@ -28,5 +65,13 @@ export class HeroService {
       .then(heroes => heroes.filter(
         hero => hero.id === id
       )[0]);
+  }
+
+  save(hero: Hero): Promise<Hero> {
+    if (hero.id) {
+      return this.put(hero);
+    }
+
+    return this.post(hero);
   }
 }
